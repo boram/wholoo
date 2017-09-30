d4d66ef0813fb64fe51948e29a3f910e3a63e8c6
Boram Yoon <boram@me.com>
2017-09-30 14:31:29 -0700

    Install postcss-loader

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,7 +30,10 @@ const config = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+          ],
         }),
       }
     ]
