37fb8b24bc4705d33d2be735c44abb8a5adc975b
Boram Yoon <boram@me.com>
2017-10-08 18:07:22 -0700

    Fix PageControllerTest

--- test/wholoo_web/controllers/page_controller_test.exs
+++ test/wholoo_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule WholooWeb.PageControllerTest do
   use WholooWeb.ConnCase
+  import Phoenix.Controller
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert view_template(conn) =~ "index.html"
   end
 end
