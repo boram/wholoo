9b5953ba244bcde9058957c3b0530a55ad98b439
Boram Yoon <boram@me.com>
2017-11-16 20:58:48 -0800

    Add title to Sign Up page

--- assets/js/components/pages/SignUp/index.js
+++ assets/js/components/pages/SignUp/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from 'common/Layout'
+import H2 from 'common/H2'
 import FormWithData from './Form'
 
 const SignUp = () => (
   <Layout>
-    <FormWithData />
+    <div>
+      <H2>Sign up</H2>
+      <FormWithData />
+    </div>
   </Layout>
 )
 
