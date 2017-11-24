1dfeb9bba3ce55f77618a9164461f5392adca0cc
Boram Yoon <boram@me.com>
2017-11-24 10:08:08 -0800

    Uninstall Mocha and Expect
    * Remove jsdom setup

--- assets/js/spec/globals.js
+++ assets/js/spec/globals.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import fetchMock from 'fetch-mock'
-import expect, { createSpy, spyOn } from 'expect'
 
 global.React = React
 global.shallow = shallow
 global.mount = mount
-global.expect = expect
-global.createSpy = createSpy
-global.spyOn = spyOn
 global.fetchMock = fetchMock
