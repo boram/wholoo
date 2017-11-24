e11c353ba032a8c3186d5bd0767f0ac706c385a9
Boram Yoon <boram@me.com>
2017-11-24 10:35:29 -0800

    Invoke Jest from npm script
    * Configure Jest to run env.js to setup test environment

--- assets/js/test/env.js
+++ assets/js/test/env.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { shallow, mount } from 'enzyme'
 import fetchMock from 'fetch-mock'
+
+Enzyme.configure({ adapter: new Adapter() })
 
 global.React = React
 global.shallow = shallow
